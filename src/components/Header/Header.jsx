bf15edd8b31d6388d904b96af763c05c6e63a994
Tintswalo <110996874+calcaventia@users.noreply.github.com>
2024-12-07 02:58:49 +0200

    header component building

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,51 @@
 import React from "react";
+import "./Header.css";
+import Logo from "../../assets/icons-img/logo.png";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header>
+      <div className="wrapper">
+        <nav>
+          <div className="logo-icon">
+            <img
+              src={Logo}
+              alt="Frontend Developer Portfolio"
+              className="logo"
+            />
+          </div>
+
+          <ul className="nav-links">
+            <li>
+              <Link to="/" className="menu-option">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" className="menu-option">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="/codingprojects" className="menu-option">
+                Dev Work
+              </Link>
+            </li>
+            <li>
+              <Link to="/designproject1" className="menu-option">
+                Design
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact" className="menu-option">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 }
 
