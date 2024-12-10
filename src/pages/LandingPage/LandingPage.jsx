22edc6072023df2c4c92c2ed7553fbd8843f78e6
Tintswalo <110996874+calcaventia@users.noreply.github.com>
2024-12-10 23:51:37 +0200

    added contact section

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -42,124 +42,136 @@ function LandingPage() {
           </div>
         </div>
 
-        {/*about-section*/}
+        <div className="content-background">
+          {/*about-section*/}
 
-        <div className="row about-container">
-          <div className="col-sm-6 order-sm-1 about-content">
-            <h2 className="section-heading">About Me</h2>
-            <p className="sub-heading">Software Developer</p>
-            <p className="about-content-body">
-              I'm a Software developer passionate about building innovative web
-              applications.
-            </p>
-            <p className="about-content-body">
-              My experience includes coursework and personal projects where I've
-              gained proficiency in frontend technologies like HTML, CSS,
-              Javascript and React.js, as well as backend technologies like
-              Node.js and Express.js.
-            </p>
-            <p className="about-content-body">
-              My aspiration is to work on projects that make a positive impact
-              in people's lives, whether through innovative user experiences or
-              solving complex problems.
-            </p>
-            <button className="medium-button">Tell me more</button>
+          <div className="row about-container">
+            <div className="col-sm-6 order-sm-1 about-content">
+              <h2 className="section-heading">About Me</h2>
+              <p className="sub-heading">Software Developer</p>
+              <p className="about-content-body">
+                I'm a Software developer passionate about building innovative
+                web applications.
+              </p>
+              <p className="about-content-body">
+                My experience includes coursework and personal projects where
+                I've gained proficiency in frontend technologies like HTML, CSS,
+                Javascript and React.js, as well as backend technologies like
+                Node.js and Express.js.
+              </p>
+              <p className="about-content-body">
+                My aspiration is to work on projects that make a positive impact
+                in people's lives, whether through innovative user experiences
+                or solving complex problems.
+              </p>
+              <button className="medium-button">Tell me more</button>
+            </div>
+            <div className="col-sm-6 order-sm-2  about-image">
+              <img
+                src={AboutImage}
+                alt=""
+                className="about-section-profile-image"
+              />
+            </div>
           </div>
-          <div className="col-sm-6 order-sm-2 d-flex justify-content-center align-items-center about-image">
-            <img
-              src={AboutImage}
-              alt=""
-              className="about-section-profile-image"
-            />
+
+          {/*technical skills-section*/}
+
+          <div className="technical-skills-container">
+            <h2 className="section-heading">Technical Skills</h2>
+            <hr className="heading-content-breaker" />
+            <div className="row t-s-container">
+              <div className="col me-3 technical-skill">HTML5</div>
+              <div className="col me-3 technical-skill">CSS3</div>
+              <div className="col me-3 technical-skill">Javascript</div>
+              <div className="col me-3 technical-skill">React</div>
+              <div className="col me-3 technical-skill">Bootstrap</div>
+              <div className="col me-3 technical-skill">MongoDB</div>
+              <div className="col technical-skill">Git</div>
+            </div>
+            <div className="row t-s-container2">
+              <div className="col me-2 technical-skill">MySQL</div>
+              <div className="col me-2 technical-skill">GitHub</div>
+              <div className="col me-2 technical-skill">Wordpress</div>
+              <div className="col me-2 technical-skill">Figma</div>
+              <div className="col-3 me-2 technical-skill">
+                UI/UX Design Principles
+              </div>
+              <div className="col technical-skill">Photopea</div>
+            </div>
+          </div>
+
+          {/*soft skills-section*/}
+
+          <div className="soft-skills-container">
+            <h2>Soft Skills</h2>
+            <hr className="heading-content-breaker" />
+            <h4>Communication Skills</h4>
+            <p className="body-text-medium">
+              I excel at effectively communicating ideas, requirements, and
+              feedback effectively with team members, ensuring clear
+              understanding and coordination on tasks and objectives.
+            </p>
+            <h4 id="heading-text">Collaboration and Teamwork</h4>
+            <p className="body-text-medium">
+              I have gained valuable experience collaborating on coding
+              projects, where I improved my ability to work as part of a team
+              and utilise various tools to communicate effectively and
+              efficientl with team members.
+            </p>
+            <h4 id="heading-text">Adaptability and Problem-Solving</h4>
+            <p className="body-text-medium">
+              I'm quick to learn and adapt to tools or technologies used by the
+              team. Debugging code and addressing usability issues are some of
+              the problem-solving skills I’ve improved.
+            </p>
+          </div>
+
+          {/*experience-section*/}
+
+          <div className="experience-container">
+            <h2>Experience</h2>
+            <hr className="heading-content-breaker" />
+            <p className="experience-body">
+              These projects demonstrate my skills with some developed
+              independently and others created through collaboration with team
+              members.
+            </p>
+            <div className="row project-gallery">
+              <div className="col-6">
+                <img src={GalleryImage1} alt="" className="gallery-images" />
+              </div>
+              <div className="col-6">
+                <img src={GalleryImage2} alt="" className="gallery-images" />
+              </div>
+            </div>
+            <div className="row project-gallery">
+              <div className="col-6">
+                <img src={GalleryImage3} alt="" className="gallery-images" />
+              </div>
+              <div className="col-6">
+                <img src={GalleryImage4} alt="" className="gallery-images" />
+              </div>
+            </div>
+            <div className="row project-gallery">
+              <div className="col-6">
+                <img src={GalleryImage5} alt="" className="gallery-images" />
+              </div>
+              <div className="col-6">
+                <img src={GalleryImage6} alt="" className="gallery-images" />
+              </div>
+              <div className="experience-button">
+                <button className="medium-button">Go to projects</button>
+              </div>
+            </div>
           </div>
         </div>
+        {/*contact-section*/}
 
-        {/*technical skills-section*/}
+        <div className="contact-container">
+          <p>Want to know more about what I do?</p>
 
-        <div className="technical-skills-container">
-          <h2 className="section-heading">Technical Skills</h2>
-          <hr className="heading-content-breaker" />
-          <div className="row t-s-container">
-            <div className="col me-3 technical-skill">HTML5</div>
-            <div className="col me-3 technical-skill">CSS3</div>
-            <div className="col me-3 technical-skill">Javascript</div>
-            <div className="col me-3 technical-skill">React</div>
-            <div className="col me-3 technical-skill">Bootstrap</div>
-            <div className="col me-3 technical-skill">MongoDB</div>
-            <div className="col technical-skill">Git</div>
-          </div>
-          <div className="row t-s-container2">
-            <div className="col me-2 technical-skill">MySQL</div>
-            <div className="col me-2 technical-skill">GitHub</div>
-            <div className="col me-2 technical-skill">Wordpress</div>
-            <div className="col me-2 technical-skill">Figma</div>
-            <div className="col-3 me-2 technical-skill">
-              UI/UX Design Principles
-            </div>
-            <div className="col technical-skill">Photopea</div>
-          </div>
-        </div>
-
-        {/*soft skills-section*/}
-
-        <div className="soft-skills-container">
-          <h2>Soft Skills</h2>
-          <hr className="heading-content-breaker" />
-          <h4>Communication Skills</h4>
-          <p className="body-text-medium">
-            I excel at effectively communicating ideas, requirements, and
-            feedback effectively with team members, ensuring clear understanding
-            and coordination on tasks and objectives.
-          </p>
-          <h4 id="heading-text">Collaboration and Teamwork</h4>
-          <p className="body-text-medium">
-            I have gained valuable experience collaborating on coding projects,
-            where I improved my ability to work as part of a team and utilise
-            various tools to communicate effectively and efficientl with team
-            members.
-          </p>
-          <h4 id="heading-text">Adaptability and Problem-Solving</h4>
-          <p className="body-text-medium">
-            I'm quick to learn and adapt to tools or technologies used by the
-            team. Debugging code and addressing usability issues are some of the
-            problem-solving skills I’ve improved.
-          </p>
-        </div>
-
-        {/*experience-section*/}
-
-        <div className="experience-container">
-          <h2>Experience</h2>
-          <hr className="heading-content-breaker" />
-          <p className="experience-body">
-            These projects demonstrate my skills with some developed
-            independently and others created through collaboration with team
-            members.
-          </p>
-          <div className="row project-gallery">
-            <div className="col-6">
-              <img src={GalleryImage1} alt="" className="gallery-images" />
-            </div>
-            <div className="col-6">
-              <img src={GalleryImage2} alt="" className="gallery-images" />
-            </div>
-          </div>
-          <div className="row project-gallery">
-            <div className="col-6">
-              <img src={GalleryImage3} alt="" className="gallery-images" />
-            </div>
-            <div className="col-6">
-              <img src={GalleryImage4} alt="" className="gallery-images" />
-            </div>
-          </div>
-          <div className="row project-gallery">
-            <div className="col-6">
-              <img src={GalleryImage5} alt="" className="gallery-images" />
-            </div>
-            <div className="col-6">
-              <img src={GalleryImage6} alt="" className="gallery-images" />
-            </div>
-          </div>
+          <button className="medium-button">Contact me</button>
         </div>
       </main>
       <Footer />
