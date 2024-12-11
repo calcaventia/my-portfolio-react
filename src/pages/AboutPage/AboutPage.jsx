479fb1d5642b0a9ea3a2e1fe0fe0f6fdb4709542
Tintswalo <110996874+calcaventia@users.noreply.github.com>
2024-12-11 23:19:42 +0200

    completed about page

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -20,7 +20,7 @@ function About() {
             </div>
           </div>
           <div className="row right-side">
-            <div className="col-6 text-bubble-right">
+            <div className="col-7 text-bubble-right">
               In another life I could be an actress, ha ha. I enjoy learning new
               languages and I’m currently learning Korean, “안녕하세요 (hello)”.
               I enjoy speaking in different accents so good luck figuring out
@@ -28,6 +28,93 @@ function About() {
             </div>
             <div className="col-1 circle-right"></div>
           </div>
+          <div className="row left-side">
+            <div className="col-1 circle-left"></div>
+            <div className="col-3 text-bubble-left">
+              What do you enjoy doing?
+            </div>
+          </div>
+          <div className="row right-side">
+            <div className="col-7 text-bubble-right">
+              Anything creative gets me fired up...there’s always something new
+              to discover. I try not to box myself in - I try to do as many
+              creative things as I can. Whether it’s drawing, designing, cooking
+              or building something...there’s always room for my mind to just
+              be.
+            </div>
+            <div className="col-1 circle-right"></div>
+          </div>
+          <div className="row left-side">
+            <div className="col-1 circle-left"></div>
+            <div className="col-4 text-bubble-left">
+              So where do you find inspiration?
+            </div>
+          </div>
+          <div className="row right-side">
+            <div className="col-7 text-bubble-right">
+              Wow...I let my senses lead me. Sometimes I find inspiration in
+              things I can see and sometimes I find it in things I can hear. I
+              learned a long time ago to be open to inspiration that comes from
+              the unconventional.
+            </div>
+            <div className="col-1 circle-right"></div>
+          </div>
+          <div className="row left-side">
+            <div className="col-1 circle-left"></div>
+            <div className="col-5 text-bubble-left">
+              Interesting...take me through your creation process.
+            </div>
+          </div>
+          <div className="row right-side">
+            <div className="col-7 text-bubble-right">
+              For as long as I can remember, my creation process always started
+              with brainstorming ideas, observing and researching for
+              inspiration, putting a concept together and then planning how I’d
+              execute the project. I also like my projects to have a
+              “Storytelling” element.
+            </div>
+            <div className="col-1 circle-right"></div>
+          </div>
+          <div className="row left-side">
+            <div className="col-1 circle-left"></div>
+            <div className="col-5 text-bubble-left">
+              When did you build your first website?
+            </div>
+          </div>
+          <div className="row right-side">
+            <div className="col-7 text-bubble-right">
+              2017. I didn’t even really think I was building a website...like a
+              developer, lol. All I wanted was a personal website for blogging
+              and I heard about Wordpress. I learned the basics on YouTube and
+              took it from there. A few weeks later and a “bit” of back pain, my
+              website was complete and live. Funny enough, after the site went
+              live, I missed developing the website, ha ha.
+            </div>
+            <div className="col-1 circle-right"></div>
+          </div>
+          <div className="row left-side">
+            <div className="col-1 circle-left"></div>
+            <div className="col-5 text-bubble-left">
+              What drew you to software development and UI/UX design?
+            </div>
+          </div>
+          <div className="row right-side">
+            <div className="col-7 text-bubble-right">
+              Discovering that I could create something people could see and
+              interact with was an incredible experience for me. My "aha" moment
+              was when I came across UI/UX design and realised that it addressed
+              my concerns about developing websites that didn’t feel “good
+              enough”. Combining frontend development with UI/UX design makes so
+              much sense to me because we’re in the digital era and so I it is
+              important for me to create products and services that genuinely
+              meet users’ needs.
+            </div>
+            <div className="col-1 circle-right"></div>
+          </div>
+        </div>
+        <div className="contact-info">
+          <p>"I'm intrigued...where can I reach you?"</p>
+          <button className="about-contact-button">Contact me</button>
         </div>
       </main>
       <Footer />
--- src/pages/DesignProjects/DesignProject1.jsx
+++ src/pages/DesignProjects/DesignProject1.jsx
@@ -1,9 +1,144 @@
 import React from "react";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import "../../pages/DesignProjects/DesignProjects.css";
+import AppImage1 from "../../assets/images/SpotBuddie-UI-1.JPG";
+import AppImage2 from "../../assets/images/SpotBuddie-UI-2.JPG";
+import AppImage3 from "../../assets/images/SpotBuddie-UI-3.JPG";
 
 function DesignProject1() {
   return (
     <div>
-      <h1>Spot Buddie Project</h1>
+      <Header />
+      <main>
+        <div className="design-content-container">
+          <div className="design-heading">
+            <h1 className="design-projects-heading">
+              D<span className="design-bold-letter">E</span>SIGN
+            </h1>
+          </div>
+          <button className="next-button">Next Project</button>
+          <div className="design-project-images">
+            <div className="row main-images">
+              <div className="col-3 app-images">
+                <img src={AppImage1} alt="" className="app-image" />
+              </div>
+              <div className="col-3 app-images">
+                <img src={AppImage2} alt="" className="app-image" />
+              </div>
+              <div className="col-3 app-images">
+                <img src={AppImage3} alt="" className="app-image" />
+              </div>
+            </div>
+          </div>
+          <h4>User-Experience Case Study</h4>
+          <p>
+            A Bluetooth-enabled mobile tracking application designed to assist
+            busy individuals to easily locate misplaced items.
+          </p>
+          <div className="problem-statement">
+            <div className="row problem-statement-section">
+              <div className="col-7 problem-statement-content">
+                <p>
+                  Busy professionals often face challenges in keeping track of
+                  essential items like car keys, wallets, or headphones,
+                  especially when juggling a demanding schedule. This can lead
+                  to unnecessary stress, wasted time, and disruption in their
+                  daily routines.
+                </p>
+                <p>
+                  For busy individuals living in a bustling city like, for
+                  example, Johannesburg, the need to stay organized is
+                  paramount. Existing solutions lack comprehensive tracking
+                  features, particularly for locating objects that are out of
+                  Bluetooth range.
+                </p>
+              </div>
+              <div className="problem-statement-video">Video</div>
+            </div>
+          </div>
+          <div className="challenges-section">
+            <p className="challenges-heading">Challenges</p>
+            <p>
+              One of the primary challenges in developing Spot Pal is addressing
+              the limitations of Bluetooth range, which can make tracking
+              ineffective when items are misplaced beyond its coverage. Another
+              significant hurdle is designing an intuitive user interface that
+              caters to busy professionals who may lack the time or patience to
+              navigate a complicated app.
+            </p>
+            <p>
+              Additionally, the app must optimize power usage to prevent
+              excessive battery drain on both the tracker and the user's mobile
+              device. Lastly, encouraging user adoption requires creating a
+              smooth onboarding experience that integrates the app into the
+              user’s daily routine without adding unnecessary complexity.
+            </p>
+          </div>
+          <div className="solution-section">
+            <p className="solution-heading">Possible Solution</p>
+            <ul>
+              <li>
+                Expand the tracking range using a satellite feature to locate
+                objects far beyond Bluetooth limits.
+              </li>
+              <li>
+                Create a user-friendly interface with simple navigation and
+                quick access to essential features.
+              </li>
+              <li>
+                Optimise the app and tracker for low energy consumption,
+                ensuring reliability without frequent recharging.
+              </li>
+              <li>
+                Design a quick, step-by-step onboarding process to familiarize
+                users with the app's features without being time-consuming.
+              </li>
+            </ul>
+          </div>
+          <div className="row project-info">
+            <div className="col-4">
+              <p>Project Duration</p>
+              <div className="row project-duration">
+                <div className="col-2 duration-number">3</div>
+                <div className="col-2 duration">weeks</div>
+              </div>
+              <div className="row project-duration">
+                <div className="col-2 screens-number">40+</div>
+                <div className="col-2 screens">screens</div>
+              </div>
+            </div>
+
+            <div className="col-8 tools-used">
+              <p>Tools Used</p>
+              <div className="row">
+                <div className="row project-duration">
+                  <div className="col-2 figma-image">3</div>
+                  <div className="col-2 figma-text">Figma</div>
+                </div>
+                <div className="row project-duration">
+                  <div className="col-2 figma-image">3</div>
+                  <div className="col-2 figma-text">Figma</div>
+                </div>
+                <div className="row project-duration">
+                  <div className="col-2 figma-image">3</div>
+                  <div className="col-2 figma-text">Figma</div>
+                </div>
+                <div className="row project-duration">
+                  <div className="col-2 figma-image">3</div>
+                  <div className="col-2 figma-text">Figma</div>
+                </div>
+                <div className="row project-duration">
+                  <div className="col-2 figma-image">3</div>
+                  <div className="col-2 figma-text">Figma</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <img src="" alt="" className="desktop-version" />
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 }
